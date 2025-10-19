dd54a88305a0eaf025f5d56ac60b6a82f0a29365
Shfaa2000 <nakourshfaa@gmail.com>
2025-10-19 12:59:59 +0300

    design login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,10 +7,18 @@ function Login() {
     <div className="">
     <div className=''>
       <img className='hearingaid' src={hearingaid} alt=''></img>
+      <p className='success'>Be Successful With Us</p>
       <img className='care' src={care} alt=''></img>
+      <p className='mda'>MDA</p>
     </div>
     <div className="login">
       <h1 className=''>Sign In</h1>
+      <p>Fill your information or register with your social account</p>
+      <input placeholder='Email Address'></input>
+      <input placeholder='Password'></input>
+      <p className='forget'>Forget your password?</p>
+      <button>Log In</button>
+      <p className='haveaccount'>Does have an account?<span>Sign in</span></p>
     </div>
     </div>
   )
